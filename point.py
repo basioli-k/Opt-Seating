5db4a3b6d7e451375d5719b9820946aea7905708
Karlo Basioli <k.basioli@gmail.com>
2020-12-08 15:26:44 +0100

    tocka i poligon

---
+++ point.py
@@ -0,0 +1,11 @@
+import math
+
+class point:
+    
+    def __init__(self,x=0,y=0):
+        self.x=x
+        self.y=y
+        
+    def distance(self,p):
+        return math.sqrt((self.x-p.x)**2 + (self.y-p.y)**2)
+    
---
+++ polygon.py
@@ -0,0 +1,9 @@
+from point import point
+
+#polygon represents a shape of the room
+
+
+class polygon:
+    def __init__(self,points=[]):
+        self.points=points
+        
