acde0aa53dbec6d4f61e0e30e6c9121c52f372c3
Karlo Basioli <k.basioli@gmail.com>
2020-12-11 17:37:10 +0100

    is polygon inside a polygon, test, test photos

--- polygon.py
+++ polygon.py
@@ -1,6 +1,6 @@
 from point import Point
 from typing import Sequence
-
+import numpy as np
 
 class Polygon:
     """
@@ -47,3 +47,52 @@ class Polygon:
 
         return contains
 
+    #function is always called by a table which cannot contain a room
+    def containedIn(self, poly):
+
+        """
+        :param poly: polygon to check
+        :return: is self contained in poly
+
+        checks if a vertex from self iz contained in poly, if it isn't then it returns false
+
+        checks if there is an intersection between between the sides of the two polygons, if they intersect return false
+
+        if both tests are passed return true
+        """
+
+        for point in self.points:   #todo improve this?
+            if point not in poly:
+                return False
+        i = 0
+        j = self.point_cnt -1
+
+        while i < self.point_cnt:
+            x1 = self.points[i].x
+            y1 = self.points[i].y
+            x2 = self.points[j].x
+            y2 = self.points[j].y
+            k = 0
+            
+            l = poly.point_cnt -1
+            while k < poly.point_cnt -1:
+                x3 = poly.points[k].x
+                y3 = poly.points[k].y
+                x4 = poly.points[l].x
+                y4 = poly.points[l].y
+                
+                A = [[x1-x2,x4-x3],[y1-y2,y4-y3]]
+                B = [x4-x2,y4-y2]
+                
+                if(np.linalg.matrix_rank(A) == 2):
+                    sol = np.linalg.solve(A,B)
+                    if( 0 <= sol[0] and sol[0] <= 1 and 0 <= sol[1] and sol[1] <= 1):
+                        return False
+                
+                l = k
+                k+=1
+            
+            j=i
+            i+=1
+        
+        return True
---
+++ test_polygon_contains_polygon.py
@@ -0,0 +1,44 @@
+from polygon import Polygon
+from point import Point
+
+
+if __name__ == "__main__":
+
+    poly = Polygon([Point(0,0),Point(4,-2),Point(8,1),Point(2,5)])
+
+    #test 1  polygon is completely inside the bigger polygon
+    test1 = Polygon([Point(4,1),Point(3,2),Point(2,0)])
+
+    assert test1.containedIn(poly)
+
+    #test 2  polygon has one vertex outside of the bigger polygon
+    test2 = Polygon([Point(4,1),Point(1,4),Point(2,0)])
+    
+    assert not test2.containedIn(poly)
+
+    #test 3  polygon is completely outside the bigger polygon
+    test3 = Polygon([Point(-3,0),Point(-2,2),Point(-1,0)])
+
+    assert not test3.containedIn(poly)
+
+    #test 4  polygon has one side on a side of the bigger polygon
+    poly = Polygon([Point(0,0),Point(4,-2),Point(8,1),Point(4,4)])
+    test4 = Polygon([Point(3,0),Point(1,1),Point(3,3)])
+
+    print(test4.containedIn(poly))
+    #returns false, should return true, check contains function
+
+    #test 5 polygon has one vertex on a side of the bigger polygon
+    test5 = Polygon([Point(3,0),Point(5,1),Point(3,3)])
+
+    print(test5.containedIn(poly))
+    #returns false because contains returns false for point F (see picture)
+
+
+    #test 6 polygon shares a side but is outside of the bigger polygon
+    test6 = Polygon([Point(1,1), Point(-3,0), Point(3,3)])
+
+    assert not test6.containedIn(poly)
+
+    
+    
