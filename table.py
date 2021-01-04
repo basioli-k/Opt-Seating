06518b160023e2d9828494c7b78d5da4e80c5f28
enaskopelja <ena.skopelja@gmail.com>
2021-01-04 17:39:56 +0100

    updated test

--- table.py
+++ table.py
@@ -5,7 +5,7 @@ from shapely.geometry import Polygon, Point
 class Table(Polygon, ABC):  # Za sad ne koristimo, ali radi ovako
     """ represents a table """
 
-    def __init__(self, length, width, lw=False):
+    def __init__(self, length=None, width=None, lw=False):
         if lw:
             super(Table, self).__init__([
                 (-length / 2, -width / 2), (-length / 2, width / 2), (length / 2, width / 2), (length / 2, -width / 2)
--- test.py
+++ test.py
@@ -3,8 +3,10 @@ from table import Table
 from shapely.geometry import Point, Polygon
 
 if __name__ == '__main__':
-    room = Room(list(map(Point, [(0, 3), (-1, 0), (0, -1), (1, 0)])))
-    table = Polygon(list(map(Point, [(-0.5, -0.5), (-0.5, 0.5), (0.5, 0.5), (0.5, -0.5)])))
+    room = Room([(0, 3), (-1, 0), (0, -1), (1, 0)])
+    table1 = Table([(-0.5, -0.5), (-0.5, 0.5), (0.5, 0.5), (0.5, -0.5)])
+    table2 = Table(0.2, 0.2, lw=True)
 
-    room.place(table)
+    room.place(table1)
+    room.place(table2)
     room.plot()
